04a8fefcb02afad01408b13a4210916b36cbe915
sunn-u <tjsntjsn20@gmail.com>
2022-01-02 18:02:47 +0900

    [2022-01-02] Setting main.py

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 # Coding by SunWoo(tjsntjsn20@gmail.com)
 
-import default_parser
-from config.config import get_config
-from engine.trainer import Trainer
+import os
+import hydra
+
+from u2net.data.types import DictConfigs, Logging
+from u2net.utils.defaults import load_yaml, merge_configs, setup_configs, save_configs
+from u2net.utils.logger import set_logger
+from u2net.engine import build_trainer, build_tester
 
 
-def setup():
-    cfg = get_config()
+def setup(config: DictConfigs) -> DictConfigs:
+    assert config["CONFIG_FILE_PATH"] is not None, \
+        'Must enter the CONFIG_FILE_PATH.\n' \
+        'ex. python main.py CONFIG_FILE_PATH=$yaml_file_path'
 
-    return cfg
+    personal_config = load_yaml(config["CONFIG_FILE_PATH"])
+    config = merge_configs(personal_config, config)
+    config = setup_configs(config)
+
+    return config
 
 
-def main():
-    cfg = setup()
+@hydra.main(config_path='u2net/config', config_name='defaults')
+def main(default_config: DictConfigs):
+    configs = setup(config=default_config)
 
-    # if cfg.eval:
-    #     pass
-    Trainer(cfg).train()
+    # Make output directory.
+    os.makedirs(configs["OUTPUT_DIR"], exist_ok=True)
+    save_configs(configs, save_dir=configs["OUTPUT_DIR"])
+
+    # Build logger.
+    logger = set_logger(level=configs["LOG"]["LEVEL"], save_dir=configs["OUTPUT_DIR"])
+
+    # Start train or infer.
+    if configs["MODEL"]["TRAIN"]:
+        trainer = build_trainer(configs, logger)
+        trainer.train()
+    else:
+        testor = build_tester(configs, logger)
+        testor.test()
 
 
 if __name__ == '__main__':
