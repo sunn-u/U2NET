b755c2b7e5b12a917042e84a2069bd4f7008b277
sunn-u <tjsntjsn20@gmail.com>
2022-01-02 20:43:34 +0900

    [2022-01-02] Add save_configs function

--- u2net/utils/defaults.py
+++ u2net/utils/defaults.py
@@ -2,7 +2,9 @@
 
 import yaml
 import torch
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
+
+from ..data.types import DictConfigs
 
 
 def load_yaml(config_path: str) -> dict:
@@ -11,11 +13,16 @@ def load_yaml(config_path: str) -> dict:
     return configs
 
 
-def merge_configs(personal_config: dict, default_config: DictConfig) -> DictConfig:
+def merge_configs(personal_config: dict, default_config: DictConfigs) -> DictConfigs:
     configs = OmegaConf.merge(default_config, personal_config)
     return configs
 
 
-def setup_configs(configs: DictConfig) -> DictConfig:
+def setup_configs(configs: DictConfigs) -> DictConfigs:
     configs["MODEL"]["DEVICE"] = 'cuda' if torch.cuda.is_available() else 'cpu'
     return configs
+
+
+def save_configs(configs: DictConfigs, save_dir: str):
+    with open(f'{save_dir}/configs.yaml', 'w') as f:
+        OmegaConf.save(config=configs, f=f.name)
