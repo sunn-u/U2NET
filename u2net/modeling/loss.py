5597c0439f70aded04805157b6cf14970fb4ab2c
sunn-u <tjsntjsn20@gmail.com>
2022-01-02 18:44:59 +0900

    [2022-01-02] Setting loss

---
+++ u2net/modeling/loss.py
@@ -0,0 +1,20 @@
+# Coding by SunWoo(tjsntjsn20@gmail.com)
+
+import torch.nn as nn
+from fvcore.common.registry import Registry
+
+LOSS_REGISTRY = Registry("LOSS")
+LOSS_REGISTRY.__doc__ = " Registry for LOSS "
+
+
+@LOSS_REGISTRY.register()
+class BinaryCrossEntropy(object):
+    def __init__(self):
+        self.loss = nn.BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
+
+    def __call__(self, gt_mask, pred_masks, fuse_mask):
+        loss = 0.
+        for pred_mask in pred_masks:
+            loss += self.criterion(pred_mask, gt_mask)
+        loss += self.criterion(fuse_mask, gt_mask)
+        return loss
