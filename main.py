0434d8d22ff92f2ff23a2ae017c9ddedf0bf9446
sunn-u <tjsntjsn20@gmail.com>
2022-01-11 00:29:49 +0900

    [2022-01-11] Edit main.py for using hydra-logging

--- main.py
+++ main.py
@@ -4,40 +4,20 @@ import os
 import hydra
 
 from u2net.data.types import DictConfigs
-from u2net.utils.defaults import load_yaml, merge_configs, setup_configs, save_configs
-from u2net.utils.logger import set_logger
 from u2net.engine import build_trainer, build_tester
 
 
-def setup(config: DictConfigs) -> DictConfigs:
-    assert config["CONFIG_FILE_PATH"] is not None, \
-        'Must enter the CONFIG_FILE_PATH.\n' \
-        'ex. python main.py CONFIG_FILE_PATH=$yaml_file_path'
-
-    personal_config = load_yaml(config["CONFIG_FILE_PATH"])
-    config = merge_configs(personal_config, config)
-    config = setup_configs(config)
-
-    return config
-
-
-@hydra.main(config_path='u2net/config', config_name='defaults')
-def main(default_config: DictConfigs):
-    configs = setup(config=default_config)
-
+@hydra.main(config_path='configs', config_name='defaults')
+def main(configs: DictConfigs) -> None:
     # Make output directory.
-    os.makedirs(configs["OUTPUT_DIR"], exist_ok=True)
-    save_configs(configs, save_dir=configs["OUTPUT_DIR"])
-
-    # Build logger.
-    logger = set_logger(level=configs["SOLVER"]["LOG"]["LEVEL"], save_dir=configs["OUTPUT_DIR"])
+    os.makedirs(configs.user.training.output_dir, exist_ok=True)
 
     # Start train or infer.
-    if configs["MODEL"]["TRAIN"]:
-        trainer = build_trainer(configs, logger)
+    if configs.user.model.train:
+        trainer = build_trainer(configs)
         trainer.train()
     else:
-        testor = build_tester(configs, logger)
+        testor = build_tester(configs)
         testor.test()
 
 
