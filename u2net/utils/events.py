bd5551e8f48e63d7545d8602e7c1c40f6d4825b0
sunn-u <tjsntjsn20@gmail.com>
2022-01-18 21:33:37 +0900

    [2022-01-18] Add events for trainer-storage

--- u2net/utils/events.py
+++ u2net/utils/events.py
@@ -1,11 +1,15 @@
 # Coding by SunWoo(tjsntjsn20@gmail.com)
 
-import datetime
 import logging
 from collections import OrderedDict, defaultdict
 
 
 _CURRENT_STORAGE_STACK = []
+
+
+def get_event_storage():
+    return _CURRENT_STORAGE_STACK[-1]
+
 
 class EventStorage(object):
     '''
@@ -21,23 +25,9 @@ class EventStorage(object):
             self.history[self.epoch][key] = value
 
     def __enter__(self):
-        _CURRENT_STORAGE_STACK.append(self.history[self.epoch])
+        _CURRENT_STORAGE_STACK.append(self)
+        return self
 
-    def __exit__(self):
-        if len(_CURRENT_STORAGE_STACK) != 1:
-            _CURRENT_STORAGE_STACK.pop(-2)
-
-
-class CommonMetricWriter(object):
-    def __init__(self, max_epochs: int):
-        self.max_epochs = max_epochs
-        self.logger = logging.getLogger(__name__)
-
-    def write(self):
-        events = _CURRENT_STORAGE_STACK[-1]
-        losses = events["losses"]
-        time = str(datetime.timedelta(seconds=int(events["time"])))
-
-        self.logger.info(
-            f"[{events.keys()}/{self.max_epochs}] losses: {losses} - time : {time}'"
-        )
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        assert _CURRENT_STORAGE_STACK[-1] == self
+        _CURRENT_STORAGE_STACK.pop()
