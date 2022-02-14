3fdef208d8dc0464c31eea8820125d6dd1e2e5d3
sunn-u <tjsntjsn20@gmail.com>
2022-02-15 00:11:43 +0900

    [2022-02-15] solve oom problem + delete temp code

--- u2net/engine/trainer/default.py
+++ u2net/engine/trainer/default.py
@@ -25,11 +25,12 @@ class Testor(object):
         model.eval()
 
         gt_list, fuse_list = [], []
-        for data, target in self.loader:
-            data = data.to(self.configs.user.model.device)
-            _, fuse_mask = model(data)
-            gt_list.append(target.to(self.configs.user.model.device))
-            fuse_list.append(fuse_mask)
+        with torch.no_grad():
+            for data, target in self.loader:
+                data = data.to(self.configs.user.model.device)
+                _, fuse_mask = model(data)
+                gt_list.append(target.to(self.configs.user.model.device))
+                fuse_list.append(fuse_mask)
 
         eval_results = self.evaluator(gt_masks=torch.cat(gt_list), pred_masks=torch.cat(fuse_list))
         return eval_results
@@ -55,11 +56,7 @@ class DefaultTrainer(TrainerBase):
             model=model,
             optimizer=optimizer,
             criterion=criterion,
-            loader=loader,
-
-            # [2022-02-14] solution for OOM.
-            test_loader=self.build_data_loader(configs, is_train=False),
-            evaluator=self.build_evaluation(configs)
+            loader=loader
         )
 
         # Settings
@@ -70,11 +67,11 @@ class DefaultTrainer(TrainerBase):
         self.eval_checkpoint_period = configs.user.training.checkpoint_period
 
         self._checkpointer = CheckPointer(max_epoch=self.max_epoch, save_dir=configs.user.training.output_dir)
-        # self._testor = Testor(
-        #     configs=configs,
-        #     loader=self.build_data_loader(configs, is_train=False),
-        #     evaluator=self.build_evaluation(configs)
-        # )
+        self._testor = Testor(
+            configs=configs,
+            loader=self.build_data_loader(configs, is_train=False),
+            evaluator=self.build_evaluation(configs)
+        )
         self.logger = logging.getLogger(__name__)
 
         self.register_hooks(self.build_hooks())
@@ -83,7 +80,7 @@ class DefaultTrainer(TrainerBase):
         hook_block = [
             hooks.EpochTimer(),
             # for after_step.
-            # hooks.EvalHook(self._testor, self.eval_checkpoint_period),
+            hooks.EvalHook(self._testor, self.eval_checkpoint_period),
             hooks.PeriodicCheckpointer(self._checkpointer, self.eval_checkpoint_period),
             hooks.CommonWriter()
         ]
